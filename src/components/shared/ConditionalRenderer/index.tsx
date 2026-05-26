bd49f6fbbd64ef58fc832877485eb313e9ce2b4f
AMIRALI <alimoradi7718@gmail.com>
2026-05-26 14:31:00 +0330

    add ConditionalRenderer-components to render a component base on a condition

---
+++ src/components/shared/ConditionalRenderer/index.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useSize } from "@/provider/widthProvider";
+
+interface ConditionalRendererProps {
+  desktop: React.ReactNode;
+  mobile: React.ReactNode;
+}
+
+export const ConditionalRenderer = ({
+  desktop,
+  mobile,
+}: ConditionalRendererProps) => {
+  const { width, isMobileClient, userAgent } = useSize();
+  console.log(width);
+  console.log(isMobileClient);
+  console.log(userAgent);
+
+  const isMobileSSR = /Android|iPhone|iPad|iPod/i.test(userAgent);
+
+  const activeIsMobile = width > 0 ? isMobileClient : isMobileSSR;
+
+  return <>{activeIsMobile ? mobile : desktop}</>;
+};
