3c65e27e6ebf6c9668bc7ec9c861752b502267dd
AMIRALI <alimoradi7718@gmail.com>
2026-05-26 17:13:16 +0330

    add persistKeys to manage cookie inside the browser

---
+++ src/core/constants/persistKeys.ts
@@ -0,0 +1,7 @@
+export const persistKeys = {
+  NEXT_RESUME_BUILDER_THEME: "next_resume_builder_theme",
+  NEXT_RESUME_BUILDER_LANGUAGE: "next_resume_builder_language",
+  NEXT_RESUME_BUILDER_RESUME_DRAFT: "next_resume_builder_resume_draft",
+  NEXT_RESUME_BUILDER_AUTH_TOKEN: "next_resume_builder_auth_token",
+  NEXT_RESUME_BUILDER_LAST_TEMPLATE: "next_resume_builder_last_template",
+} as const;
