aaa62f3c2bab056de0ab242030938442d2659eba
AMIRALI <alimoradi7718@gmail.com>
2026-05-26 17:30:36 +0330

    add some text into common.json file and use it in Header.tsx

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -11,7 +11,7 @@ function Header() {
   const { t } = useTranslation(lng, "common");
   const { colors } = useThemeColors();
   return (
-    <div className="flex items-center justify-between w-full  p-5 ">
+    <div className="flex items-center justify-between w-full  px-8 py-6 ">
       <div className="flex items-center gap-1">
         <SiteLogo size={50} color={colors.brand?.brandPrimary} />
         <CustomText
@@ -22,7 +22,12 @@ function Header() {
           {t("siteName")}
         </CustomText>
       </div>
-      <div>right</div>
+      <nav className="flex items-center justify-between gap-5 capitalize">
+        <div>{t("register")}</div>
+        <div>{t("login")}</div>
+        <div>{t("blog")}</div>
+        <div>{t("aboutUs")}</div>
+      </nav>
     </div>
   );
 }
