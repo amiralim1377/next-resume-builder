cc1d8a6d8e8d597c1c20e2362ad3e0282e9b6aa4
AMIRALI <alimoradi7718@gmail.com>
2026-05-26 11:15:57 +0330

    add  useTranslation as getTranslation to test traslation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>app</div>;
+import { useTranslation as getTranslation } from "@/lib/i18n/server";
+import { Language } from "@/lib/i18n/settings";
+
+interface IHomePageProps {
+  params: Promise<{ locale: Language }>;
 }
+
+async function Home({ params }: IHomePageProps) {
+  const { locale } = await params;
+
+  const { t } = await getTranslation(locale, "home");
+  return <div>{t("ansar")}</div>;
+}
+
+export default Home;
