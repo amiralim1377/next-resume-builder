84e78e138025732f9da1f42bb989ffc108b77557
AMIRALI <alimoradi7718@gmail.com>
2026-05-27 19:47:12 +0330

    change some style in header-components

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -18,7 +18,7 @@ function Header() {
     switchTheme();
   };
   return (
-    <div className="flex items-center justify-between w-full bg-ui-bg   px-8 py-6 ">
+    <div className="flex items-center justify-between w-full bg-ui-surface   px-8 py-6 ">
       <div className="flex items-center gap-1">
         <SiteLogo size={50} color={colors.brand?.brandPrimary} />
         <CustomText
@@ -34,16 +34,35 @@ function Header() {
           </button>
         </div>
       </div>
-      <nav className="flex items-center justify-between gap-5 capitalize text-lg font-light  ">
-        <CustomLink href={"#"}>{t("login")}</CustomLink>
-
-        <CustomLink href={"#"}>{t("register")}</CustomLink>
-
-        <CustomLink href={"#"}>{t("blog")}</CustomLink>
+      <nav className="flex items-center justify-between gap-5 capitalize text-lg font-light">
+        <CustomLink
+          href={"#"}
+          className="relative text-text-secondary transition-all duration-300 ease-in-out hover:text-brandLight hover:scale-105 active:scale-95"
+        >
+          {t("login")}
+        </CustomLink>
 
         <CustomLink
           href={"#"}
-          className="bg-brand-primary text-white px-4 py-2 rounded-xl hover:bg-brand-primary/60 transition-all"
+          className="relative text-text-secondary transition-all duration-300 ease-in-out hover:text-brandLight hover:scale-105 active:scale-95"
+        >
+          {t("register")}
+        </CustomLink>
+
+        <CustomLink
+          href={"#"}
+          className="relative text-text-secondary transition-all duration-300 ease-in-out hover:text-brandLight hover:scale-105 active:scale-95"
+        >
+          {t("blog")}
+        </CustomLink>
+
+        {/* Primary Action Button */}
+        <CustomLink
+          href={"#"}
+          className="bg-brandPrimary text-brandText px-5 py-2 rounded-xl 
+               transition-all duration-300 ease-in-out 
+               hover:bg-brandHover hover:shadow-lg hover:shadow-brandPrimary/20 
+               active:scale-95"
         >
           {t("aboutUs")}
         </CustomLink>
