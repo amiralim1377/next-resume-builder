244610f595d67032fd6d87b2500a5060c166d515
AMIRALI <alimoradi7718@gmail.com>
2026-05-28 13:11:51 +0330

    remove extra className and add fragment

--- src/components/pages/HomePage/components/SuccessStories/index.tsx
+++ src/components/pages/HomePage/components/SuccessStories/index.tsx
@@ -3,12 +3,13 @@
 import { CustomText } from "@/components/ui/customText";
 import { useTranslation } from "@/lib/i18n/client";
 import { useLang } from "@/provider/lngProvider";
+import { SuccessStoriesList } from "../SuccessStoriesList";
 
 function SuccessStories() {
   const { lng } = useLang();
   const { t } = useTranslation(lng, "home");
   return (
-    <div className="flex items-center justify-center">
+    <>
       <div className="flex flex-col items-center justify-center gap-y-10">
         <CustomText className="text-text-secondary block text-5xl font-bold tracking-tight">
           {t("successStories")}
@@ -17,7 +18,8 @@ function SuccessStories() {
           {t("successStoriesContentMoto")}
         </CustomText>
       </div>
-    </div>
+      <SuccessStoriesList t={t} />
+    </>
   );
 }
 
