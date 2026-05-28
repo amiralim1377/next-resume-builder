aab141aadec01abbce627efb4df3163594aa9980
AMIRALI <alimoradi7718@gmail.com>
2026-05-28 09:50:47 +0330

    add cn function to HeroSectionContent

--- src/components/pages/HomePage/components/HeroSectionContent/index.tsx
+++ src/components/pages/HomePage/components/HeroSectionContent/index.tsx
@@ -6,17 +6,16 @@ import { useTranslation } from "@/lib/i18n/client";
 import { useLang } from "@/provider/lngProvider";
 import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
 import { CheckCircle } from "@/components/svg/checkCircle";
+import { cn } from "@/utils/cn";
 
 function HeroSectionContent() {
   const { lng } = useLang();
   const { t } = useTranslation(lng, "home");
   const { colors, theme } = useThemeColors();
   return (
-    <div className="flex flex-col items-start w-1/2 space-y-4 ">
+    <div className="flex w-1/2 flex-col items-start space-y-4">
       <CustomBadge
-        className={
-          "text-xs rounded-xl  bg-brandLight text-reverse-text-primary"
-        }
+        className={"bg-brandLight text-reverse-text-primary rounded-xl text-xs"}
       >
         {t("v2Available")}
       </CustomBadge>
@@ -24,48 +23,67 @@ function HeroSectionContent() {
         <div>
           {lng == "fa" ? (
             <CustomText
-              className={`capitalize font-black  text-6xl leading-tight ${theme === "dark" ? "text-brandPrimary" : ""}  `}
+              className={cn(
+                "text-6xl leading-tight font-black capitalize",
+                theme === "dark" && "text-brandPrimary",
+              )}
             >
               {t("inMinutes")}
             </CustomText>
           ) : (
-            <CustomText className="font-black text-6xl leading-tight capitalize  ">
+            <CustomText
+              className={cn(
+                "text-6xl leading-tight font-black capitalize",
+                theme === "dark" && "text-brandPrimary",
+              )}
+            >
               {t("craftYourResume")}
             </CustomText>
           )}
         </div>
 
-        <div className="flex gap-3   ">
+        <div className="flex gap-3">
           <CustomText
-            className={`font-black text-6xl leading-tight capitalize ${theme === "light" ? "text-brandPrimary" : "text-white"} `}
+            className={cn(
+              "text-6xl leading-tight font-black capitalize",
+              theme === "light" ? "text-brandPrimary" : "text-white",
+            )}
           >
             {t("resume")}
           </CustomText>
           {lng == "en" ? (
-            <CustomText className="font-black text-6xl leading-tight capitalize  ">
+            <CustomText
+              className={cn(
+                "text-6xl leading-tight font-black capitalize",
+                theme === "dark" && "text-brandPrimary",
+              )}
+            >
               {t("inMinutes")}
             </CustomText>
           ) : (
             <CustomText
-              className={`font-black  text-6xl leading-tight capitalize ${theme === "dark" ? "text-brandPrimary" : ""} `}
+              className={cn(
+                "text-6xl leading-tight font-black capitalize",
+                theme === "dark" ? "text-brandPrimary" : "",
+              )}
             >
               {t("craftYourResume")}
             </CustomText>
           )}
         </div>
-        <CustomText className="capitalize text-text-secondary ">
+        <CustomText className="text-text-secondary capitalize">
           {t("heroSectionMotoContent")}
         </CustomText>
       </div>
       <div className="flex items-center gap-6">
         <CustomLink
-          className="capitalize px-6 py-3 rounded-lg text-white bg-linear-to-r from-brandPrimary to-state-info transition-all hover:-translate-y-1"
+          className="from-brandPrimary to-state-info rounded-lg bg-linear-to-r px-6 py-3 text-white capitalize transition-all hover:-translate-y-1"
           href={"#"}
         >
           {t("buildMyResume")}
         </CustomLink>
         <CustomLink
-          className="capitalize px-6 py-3 rounded-lg border border-text-muted text-text-primary hover:bg-brand-accent "
+          className="border-text-muted text-text-primary hover:bg-brand-accent rounded-lg border px-6 py-3 capitalize"
           href={"#"}
         >
           {t("login")}
@@ -74,13 +92,13 @@ function HeroSectionContent() {
       <div className="flex items-center gap-6">
         <div className="flex items-center gap-1">
           <CheckCircle size={14} color={colors.state?.success} />
-          <CustomText className="text-sm  text-text-secondary">
+          <CustomText className="text-text-secondary text-sm">
             {t("totalFree")}
           </CustomText>
         </div>
         <div className="flex items-center gap-1">
           <CheckCircle size={14} color={colors.state?.success} />
-          <CustomText className="text-sm  text-text-secondary">
+          <CustomText className="text-text-secondary text-sm">
             {t("atsFriendly")}
           </CustomText>
         </div>
