66d3b23470c9c177d08b4ba2748b8ed2d166d1dc
AMIRALI <alimoradi7718@gmail.com>
2026-05-30 21:48:21 +0330

    remove ConditionalRenderer from home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,7 @@
 import { HomePage } from "@/components/pages/HomePage";
-import { ConditionalRenderer } from "@/components/shared/ConditionalRenderer";
-import { Language } from "@/lib/i18n/settings";
 
-interface IHomePageProps {
-  params: Promise<{ locale: Language }>;
-}
-
-async function Home({ params }: IHomePageProps) {
-  return <ConditionalRenderer mobile={<HomePage />} desktop={<HomePage />} />;
+async function Home() {
+  return <HomePage />;
 }
 
 export default Home;
