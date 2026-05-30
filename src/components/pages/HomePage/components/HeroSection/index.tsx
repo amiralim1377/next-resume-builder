e04b11e84f56034e3bdb57b541e794abd056f83d
AMIRALI <alimoradi7718@gmail.com>
2026-05-30 21:58:16 +0330

    render HeroSectionImage base on width

--- src/components/pages/HomePage/components/HeroSection/index.tsx
+++ src/components/pages/HomePage/components/HeroSection/index.tsx
@@ -1,11 +1,15 @@
+"use client";
+import { useWindowSize } from "@/hooks/useWindowSize";
 import { HeroSectionContent } from "../HeroSectionContent";
 import { HeroSectionImage } from "../HeroSectionImage/HeroSectionImage";
 
 const HeroSection = () => {
+  const { width } = useWindowSize();
+
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex flex-col items-center justify-center md:flex-row">
       <HeroSectionContent />
-      <HeroSectionImage />
+      {width >= 1024 && <HeroSectionImage />}
     </div>
   );
 };
