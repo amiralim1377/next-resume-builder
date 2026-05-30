ff3c1b199129e1805bba0c2b94015a4cb66923b3
AMIRALI <alimoradi7718@gmail.com>
2026-05-30 11:57:07 +0330

    footer component was added

---
+++ src/components/layout/footer/index.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <div className="bg-brandFooter mx-auto max-w-300 py-16">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis numquam
+      explicabo amet culpa minus sunt minima quae est, voluptas vel soluta
+      voluptate ducimus accusamus possimus. Earum consectetur unde voluptatum
+      eaque!
+    </div>
+  );
+}
+
+export { Footer };
