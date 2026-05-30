abd77be4b681f0f260d0421e9e19d881f38b90aa
AMIRALI <alimoradi7718@gmail.com>
2026-05-30 23:50:23 +0330

    base on lng change padding propery

--- src/components/pages/HomePage/components/HeroSectionImage/HeroSectionImage.tsx
+++ src/components/pages/HomePage/components/HeroSectionImage/HeroSectionImage.tsx
@@ -1,14 +1,21 @@
+"use client";
 import Image from "next/image";
 import heroSectionImage from "@public/images/hero-illustration.webp";
+import { useLang } from "@/provider/lngProvider";
+import { cn } from "@/utils/cn";
 
 function HeroSectionImage() {
+  const { lng } = useLang();
   return (
     <div className="relative hidden w-2/5 overflow-hidden rounded-xl lg:block">
       <Image
         src={heroSectionImage}
         alt="Hero Illustration"
         priority
-        className="animate-float h-auto w-full rounded-lg p-4 pt-0 pb-0"
+        className={cn(
+          "animate-float h-auto w-full rounded-lg p-4 pt-0 pb-0",
+          lng === "en" ? "pr-0" : "pl-0",
+        )}
       />
     </div>
   );
