a98631d9b969219d1c1bfc2a856d72c9e8c23d97
AMIRALI <alimoradi7718@gmail.com>
2026-05-31 22:21:41 +0330

    add cn function and add CustomButton for each sidebar button

--- src/components/layout/panelSidebar/index.tsx
+++ src/components/layout/panelSidebar/index.tsx
@@ -1,10 +1,28 @@
+"use client";
 import { CustomAvatar } from "@/components/ui/customAvatar";
+import { CustomButton } from "@/components/ui/customButton";
+import { useTranslation } from "@/lib/i18n/client";
+import { useLang } from "@/provider/lngProvider";
+import { cn } from "@/utils/cn";
 
 const PanelSidebar = () => {
+  const { lng } = useLang();
+  const { t } = useTranslation(lng, "common");
   return (
-    <div>
+    <aside
+      className={cn(
+        "border-ui-border flex w-full flex-col justify-center gap-3 px-8 py-6",
+        lng === "fa" ? "border-l" : "border-r",
+      )}
+    >
       <CustomAvatar name="AMIRALI" />
-    </div>
+      <CustomButton>{t("buildNewResume")}</CustomButton>
+      <CustomButton variant="primary">{t("dashboard")}</CustomButton>
+      <CustomButton>{t("myResumes")}</CustomButton>
+      <CustomButton>{t("helpGuide")}</CustomButton>
+      <CustomButton>{t("settings")}</CustomButton>
+      <CustomButton variant="outlined-negative">{t("exit")}</CustomButton>
+    </aside>
   );
 };
 
