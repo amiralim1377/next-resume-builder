79bf42ef5a0cc8c221e946bc17e2e9570a80da70
AMIRALI <alimoradi7718@gmail.com>
2026-06-01 10:44:50 +0330

    add socialLinks-constant instead of hard-coding

--- src/components/layout/footer/components/connectWithUs/index.tsx
+++ src/components/layout/footer/components/connectWithUs/index.tsx
@@ -1,16 +1,14 @@
 "use client";
-import { GitHubSvg } from "@/components/svg/gitHubSvg";
 import { LinkdinSvg } from "@/components/svg/linkdinSvg";
 import { CustomLabel } from "@/components/ui/customLabel";
 import { CustomLink } from "@/components/ui/customLink";
+import { socialLinks } from "@/core/constants/socialLinks";
 import { useTranslation } from "@/lib/i18n/client";
 import { useLang } from "@/provider/lngProvider";
 
 function ConnectWithUs() {
   const { lng } = useLang();
   const { t } = useTranslation(lng, "footer");
-  const amiraliMoradiLinkdin = "https://www.linkedin.com/in/amiralimoradi/";
-  // const amiraliMoradiGitHub = "https://github.com/amiralim1377";
 
   return (
     <div className="flex w-fit flex-col items-start">
@@ -23,7 +21,7 @@ function ConnectWithUs() {
         {t("connectWithUs")}
       </CustomLabel>
       <div className="mt-4 lg:mt-8">
-        <CustomLink href={amiraliMoradiLinkdin}>
+        <CustomLink href={socialLinks.linkedin}>
           <LinkdinSvg size={25} />
         </CustomLink>
       </div>
