8102f7309fe0d560c3dc40b8da9622f53f838cb7
AMIRALI <alimoradi7718@gmail.com>
2026-06-04 16:42:24 +0330

    add some property to resume.schema

--- src/features/resume/schemas/resume.schema.ts
+++ src/features/resume/schemas/resume.schema.ts
@@ -4,24 +4,29 @@ export const basicInfoSchema = z.object({
   firstName: z.string().min(1, "required"),
   lastName: z.string().min(1, "required"),
   jobTitle: z.string().optional(),
+  sex: z.enum(["male", "female", ""]).refine(Boolean, {
+    message: "لطفاً جنسیت را انتخاب کنید",
+  }),
   email: z.email("Invalid email format"),
+  birthday: z.object({
+    day: z.string(),
+    month: z.string(),
+    year: z.string(),
+  }),
+  militaryServiceStatus: z.enum(["completed", "exempt", "in_progress", ""]),
+  maritalStatus: z.enum(["single", "married", ""]),
+  mobileNumber: z.string().regex(/^(09\d{9}|\+989\d{9}|0989\d{9})$/, {
+    message:
+      "شماره موبایل وارد شده معتبر نیست. لطفاً با فرمت‌های 09xxxxxxxxx، 0989xxxxxxxxx یا +989xxxxxxxxx وارد کنید.",
+  }),
+
   phone: z.string().min(10, "Invalid phone number"),
-  sex: z.enum(["male", "female", "other", "prefer_not_to_say"]),
-  summary: z.string().min(1, "required"),
-  birthday: z.iso.datetime(),
-  militaryServiceStatus: z.enum([
-    "completed",
-    "exempt",
-    "in_progress",
-    "not_applicable",
-  ]),
-  maritalStatus: z.enum([
-    "single",
-    "married",
-    "divorced",
-    "widowed",
-    "separated",
-  ]),
+  webSite: z.url("Please enter a valid website URL").optional(),
+  country: z.string(),
+  province: z.string(),
+  city: z.string(),
+  address: z.string(),
+  summary: z.string().optional(),
 });
 
 export const resumeSchema = z.object({
@@ -29,6 +34,7 @@ export const resumeSchema = z.object({
 });
 
 export type ResumeFormValues = z.infer<typeof resumeSchema>;
+export type BasicInfoValues = z.infer<typeof basicInfoSchema>;
 
 // eslint-disable-next-line
 export const STEP_FIELDS: Record<number, any[]> = {
