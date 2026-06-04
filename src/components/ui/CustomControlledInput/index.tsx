f1d4e7471e4e87395470f4f37f1e626c4cbe5179
AMIRALI <alimoradi7718@gmail.com>
2026-06-04 16:34:13 +0330

    add CustomControlledInput

---
+++ src/components/ui/CustomControlledInput/index.tsx
@@ -0,0 +1,42 @@
+import { ResumeFormValues } from "@/features/resume/schemas/resume.schema";
+import { Controller, Path, useFormContext } from "react-hook-form";
+import { CustomInput } from "../CustomInput";
+
+type CustomControlledInputProps = {
+  name: Path<ResumeFormValues>;
+  label: string;
+} & Omit<
+  React.ComponentProps<typeof CustomInput>,
+  "name" | "label" | "error" | "value" | "onChange" | "onBlur" | "ref"
+>;
+const CustomControlledInput = ({
+  name,
+  label,
+  ...props
+}: CustomControlledInputProps) => {
+  const { control } = useFormContext<ResumeFormValues>();
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState: { error } }) => {
+        const { value, onChange, onBlur, ref, ...safeField } = field;
+        return (
+          <CustomInput
+            label={label}
+            error={error?.message}
+            value={value as string | number | readonly string[] | undefined}
+            onChange={onChange}
+            onBlur={onBlur}
+            ref={ref}
+            {...safeField}
+            {...props}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export { CustomControlledInput };
