8c0f06f14081c4668a3d8989356de3544b94d32e
AMIRALI <alimoradi7718@gmail.com>
2026-06-04 16:37:59 +0330

    add monthsData-array

---
+++ src/core/data/monthsData.ts
@@ -0,0 +1,64 @@
+const monthsData = [
+  {
+    month_en: "April",
+    month_fa: "آوریل",
+    month_shamsi: "فروردین",
+  },
+  {
+    month_en: "May",
+    month_fa: "مه",
+    month_shamsi: "اردیبهشت",
+  },
+  {
+    month_en: "June",
+    month_fa: "ژوئن",
+    month_shamsi: "خرداد",
+  },
+  {
+    month_en: "July",
+    month_fa: "جولای",
+    month_shamsi: "تیر",
+  },
+  {
+    month_en: "August",
+    month_fa: "آگوست",
+    month_shamsi: "مرداد",
+  },
+  {
+    month_en: "September",
+    month_fa: "سپتامبر",
+    month_shamsi: "شهریور",
+  },
+  {
+    month_en: "October",
+    month_fa: "اکتبر",
+    month_shamsi: "مهر",
+  },
+  {
+    month_en: "November",
+    month_fa: "نوامبر",
+    month_shamsi: "آبان",
+  },
+  {
+    month_en: "December",
+    month_fa: "دسامبر",
+    month_shamsi: "آذر",
+  },
+  {
+    month_en: "January",
+    month_fa: "ژانویه",
+    month_shamsi: "دی",
+  },
+  {
+    month_en: "February",
+    month_fa: "فوریه",
+    month_shamsi: "بهمن",
+  },
+  {
+    month_en: "March",
+    month_fa: "مارس",
+    month_shamsi: "اسفند",
+  },
+];
+
+export { monthsData };
