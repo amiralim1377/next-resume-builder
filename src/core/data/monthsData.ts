549546216c0cee780c87bd39b53edeb852c20a16
AMIRALI <alimoradi7718@gmail.com>
2026-06-06 19:34:37 +0330

    divide GregorianDataType and JalaliDataType to manage it better

--- src/core/data/monthsData.ts
+++ src/core/data/monthsData.ts
@@ -1,70 +1,106 @@
-type MonthsDataType = {
-  month_en: string;
+type JalaliDataType = {
   month_fa: string;
   month_shamsi: string;
 };
 
-const monthsData: MonthsDataType[] = [
-  {
-    month_en: "April",
-    month_fa: "آوریل",
-    month_shamsi: "فروردین",
-  },
-  {
-    month_en: "May",
-    month_fa: "مه",
-    month_shamsi: "اردیبهشت",
-  },
-  {
-    month_en: "June",
-    month_fa: "ژوئن",
-    month_shamsi: "خرداد",
-  },
-  {
-    month_en: "July",
-    month_fa: "جولای",
-    month_shamsi: "تیر",
-  },
-  {
-    month_en: "August",
-    month_fa: "آگوست",
-    month_shamsi: "مرداد",
-  },
-  {
-    month_en: "September",
-    month_fa: "سپتامبر",
-    month_shamsi: "شهریور",
-  },
-  {
-    month_en: "October",
-    month_fa: "اکتبر",
-    month_shamsi: "مهر",
-  },
-  {
-    month_en: "November",
-    month_fa: "نوامبر",
-    month_shamsi: "آبان",
-  },
-  {
-    month_en: "December",
-    month_fa: "دسامبر",
-    month_shamsi: "آذر",
-  },
-  {
-    month_en: "January",
-    month_fa: "ژانویه",
-    month_shamsi: "دی",
-  },
-  {
-    month_en: "February",
-    month_fa: "فوریه",
-    month_shamsi: "بهمن",
-  },
-  {
-    month_en: "March",
-    month_fa: "مارس",
-    month_shamsi: "اسفند",
-  },
-];
+type GregorianDataType = {
+  month_en: string;
+};
+
+export type MonthsDataType = {
+  jalali: JalaliDataType[];
+  gregorian: GregorianDataType[];
+};
+
+const monthsData: MonthsDataType = {
+  jalali: [
+    {
+      month_fa: "آوریل",
+      month_shamsi: "فروردین",
+    },
+    {
+      month_fa: "مه",
+      month_shamsi: "اردیبهشت",
+    },
+    {
+      month_fa: "ژوئن",
+      month_shamsi: "خرداد",
+    },
+    {
+      month_fa: "جولای",
+      month_shamsi: "تیر",
+    },
+    {
+      month_fa: "آگوست",
+      month_shamsi: "مرداد",
+    },
+    {
+      month_fa: "سپتامبر",
+      month_shamsi: "شهریور",
+    },
+    {
+      month_shamsi: "مهر",
+      month_fa: "اکتبر",
+    },
+    {
+      month_fa: "نوامبر",
+      month_shamsi: "آبان",
+    },
+    {
+      month_fa: "دسامبر",
+      month_shamsi: "آذر",
+    },
+    {
+      month_fa: "ژانویه",
+      month_shamsi: "دی",
+    },
+    {
+      month_fa: "فوریه",
+      month_shamsi: "بهمن",
+    },
+    {
+      month_fa: "مارس",
+      month_shamsi: "اسفند",
+    },
+  ],
+  gregorian: [
+    {
+      month_en: "January",
+    },
+    {
+      month_en: "February",
+    },
+    {
+      month_en: "March",
+    },
+    {
+      month_en: "April",
+    },
+    {
+      month_en: "May",
+    },
+    {
+      month_en: "June",
+    },
+    {
+      month_en: "July",
+    },
+    {
+      month_en: "August",
+    },
+    {
+      month_en: "September",
+    },
+    {
+      month_en: "October",
+    },
+    {
+      month_en: "November",
+    },
+    {
+      month_en: "December",
+    },
+  ],
+};
 
 export { monthsData };
