8e05b23c2164e1da516896becd9e1489a18a7e63
AMIRALI <alimoradi7718@gmail.com>
2026-06-06 19:37:22 +0330

    add EducationSection in EducationStep

--- src/features/resume/components/steps/educationStep/index.tsx
+++ src/features/resume/components/steps/educationStep/index.tsx
@@ -1,5 +1,15 @@
+import { useLang } from "@/provider/lngProvider";
+import { EducationSection } from "./components/EducationSection";
+import { useTranslation } from "@/lib/i18n/client";
+
 function EducationStep() {
-  return <div>EducationStep</div>;
+  const { lng } = useLang();
+  const { t } = useTranslation(lng, "form");
+  return (
+    <div className="flex w-full flex-col space-y-8">
+      <EducationSection t={t} lng={lng} />
+    </div>
+  );
 }
 
 export { EducationStep };
