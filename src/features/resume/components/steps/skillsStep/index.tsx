7d00e3594d8411b5d1021bd8d96df4f3dd3d0e90
AMIRALI <alimoradi7718@gmail.com>
2026-06-07 21:49:24 +0330

    add CoursesCertificationsSection into SkillsStep

--- src/features/resume/components/steps/skillsStep/index.tsx
+++ src/features/resume/components/steps/skillsStep/index.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "@/lib/i18n/client";
 import { useLang } from "@/provider/lngProvider";
 import { LanguageSection } from "./components/LanguageSection";
 import { CoreSkillsSection } from "./components/CoreSkillsSection";
+import { CoursesCertificationsSection } from "./components/CoursesCertificationsSection";
 
 function SkillsStep() {
   const { lng } = useLang();
@@ -10,7 +11,7 @@ function SkillsStep() {
     <div>
       <LanguageSection t={t} lng={lng} />
       <CoreSkillsSection t={t} lng={lng} />
-      <LanguageSection t={t} lng={lng} />
+      <CoursesCertificationsSection t={t} lng={lng} />
     </div>
   );
 }
