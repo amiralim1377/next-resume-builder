69438e9c766d4c35e900552a07bfde4c750b56e6
AMIRALI <alimoradi7718@gmail.com>
2026-06-07 21:48:50 +0330

    add DISPLAY_MODE_DATA

---
+++ src/core/data/languageDisplayModeData.ts
@@ -0,0 +1,63 @@
+// type DisplayMode =
+//   | "overall-descriptive"
+//   | "overall-graphic"
+//   | "overall-cefr"
+//   | "breakdown-descriptive"
+//   | "breakdown-graphic"
+//   | "breakdown-cefr";
+
+export const DISPLAY_MODE_DATA = {
+  fa: [
+    {
+      value: "overall-descriptive",
+      text: "سطح کلی - توصیفی",
+    },
+    {
+      value: "overall-graphic",
+      text: "سطح کلی - گرافیکی",
+    },
+    {
+      value: "overall-cefr",
+      text: "سطح کلی - CEFR",
+    },
+    {
+      value: "breakdown-descriptive",
+      text: "تفکیک مهارت‌ها - توصیفی",
+    },
+    {
+      value: "breakdown-graphic",
+      text: "تفکیک مهارت‌ها - گرافیکی",
+    },
+    {
+      value: "breakdown-cefr",
+      text: "تفکیک مهارت‌ها - CEFR",
+    },
+  ],
+
+  en: [
+    {
+      value: "overall-descriptive",
+      text: "Overall Level - Descriptive",
+    },
+    {
+      value: "overall-graphic",
+      text: "Overall Level - Graphic",
+    },
+    {
+      value: "overall-cefr",
+      text: "Overall Level - CEFR",
+    },
+    {
+      value: "breakdown-descriptive",
+      text: "Skill Breakdown - Descriptive",
+    },
+    {
+      value: "breakdown-graphic",
+      text: "Skill Breakdown - Graphic",
+    },
+    {
+      value: "breakdown-cefr",
+      text: "Skill Breakdown - CEFR",
+    },
+  ],
+};
