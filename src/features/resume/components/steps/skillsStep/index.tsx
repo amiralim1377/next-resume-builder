df00375d8d9247343a00cb0f6006bd26bce1edec
AMIRALI <alimoradi7718@gmail.com>
2026-06-07 13:17:57 +0330

    add SkillsStep and related components

--- src/features/resume/components/steps/skillsStep/index.tsx
+++ src/features/resume/components/steps/skillsStep/index.tsx
@@ -1,5 +1,18 @@
+import { useTranslation } from "@/lib/i18n/client";
+import { useLang } from "@/provider/lngProvider";
+import { LanguageSection } from "./components/LanguageSection";
+import { CoreSkillsSection } from "./components/CoreSkillsSection";
+
 function SkillsStep() {
-  return <div>skillsStep</div>;
+  const { lng } = useLang();
+  const { t } = useTranslation(lng, "form");
+  return (
+    <div>
+      <LanguageSection t={t} lng={lng} />
+      <CoreSkillsSection t={t} lng={lng} />
+      <LanguageSection t={t} lng={lng} />
+    </div>
+  );
 }
 
 export { SkillsStep };
