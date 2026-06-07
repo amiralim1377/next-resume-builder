3030ce93ca3ba574afc60af5bf60e77f1d475871
AMIRALI <alimoradi7718@gmail.com>
2026-06-07 13:18:12 +0330

    add CoursesCertificationsSection

---
+++ src/features/resume/components/steps/skillsStep/components/CoursesCertificationsSection/index.tsx
@@ -0,0 +1,14 @@
+import { Language } from "@/lib/i18n/settings";
+import { TFunction } from "i18next";
+
+type CoursesCertificationsSectionProps = {
+  t: TFunction<string, undefined>;
+  lng: Language;
+};
+
+const CoursesCertificationsSection =
+  ({}: CoursesCertificationsSectionProps) => {
+    return <div>CoursesCertificationsSection</div>;
+  };
+
+export { CoursesCertificationsSection };
