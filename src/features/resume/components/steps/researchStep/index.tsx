1ac0b2ff2104c2a1ada8effe66fec74f3d4c3577
AMIRALI <alimoradi7718@gmail.com>
2026-06-10 10:59:28 +0330

    render fileds with help of useFieldArray-hook in ResearchStep

--- src/features/resume/components/steps/researchStep/index.tsx
+++ src/features/resume/components/steps/researchStep/index.tsx
@@ -1,5 +1,53 @@
+import { CustomButton } from "@/components/ui/CustomButton";
+import { ResumeFormValues } from "@/features/resume/schemas/resume.schema";
+import { useTranslation } from "@/lib/i18n/client";
+import { useLang } from "@/provider/lngProvider";
+import { useFieldArray } from "react-hook-form";
+import { ResearchItem } from "./components/ResearchItem";
+import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
+
 function ResearchStep() {
-  return <div>ResearchStep</div>;
+  const { lng } = useLang();
+  const { t } = useTranslation(lng, "form");
+  const { fields, append, remove } = useFieldArray<
+    ResumeFormValues,
+    "research"
+  >({
+    name: "research",
+  });
+
+  const onRowAdd = () => {
+    append({
+      publicationMonth: "",
+      publicationYear: "",
+      publisher: "",
+      researchTitle: "",
+      researchUrl: "",
+    });
+  };
+
+  const onDelete = (index: number) => {
+    remove(index);
+  };
+
+  return (
+    <>
+      <div className="flex w-full flex-col space-y-8">
+        {fields.map((field, index) => (
+          <CustomResumeCardComponents calssName="mt-2" key={field.id}>
+            <ResearchItem
+              t={t}
+              lng={lng}
+              onDelete={onDelete}
+              key={field.id}
+              index={index}
+            />
+          </CustomResumeCardComponents>
+        ))}
+      </div>
+      <CustomButton onClick={onRowAdd}>ADD</CustomButton>
+    </>
+  );
 }
 
 export { ResearchStep };
