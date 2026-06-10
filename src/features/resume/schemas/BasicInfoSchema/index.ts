9b0fa08c7bde272d5a55a1190b8d2edb87228f4f
AMIRALI <alimoradi7718@gmail.com>
2026-06-10 11:06:21 +0330

    add more validation to createBasicInfoSchema

--- src/features/resume/schemas/BasicInfoSchema/index.ts
+++ src/features/resume/schemas/BasicInfoSchema/index.ts
@@ -7,9 +7,44 @@ export const MILITARY_OPTIONS = ["completed", "exempt", "inProgress"] as const;
 
 export const createBasicInfoSchema = (t: TFunction<string, undefined>) => {
   return z.object({
-    firstName: z.string().min(1, { message: t("firstNamerequired") }),
-    lastName: z.string().min(1, { message: t("lastNamerequired") }),
-    jobTitle: z.string().min(1, { message: t("jobTitlerequired") }),
+    firstName: z
+      .string()
+      .min(1, { message: t("firstNamerequired") })
+      .refine((value) => !/^\d+$/.test(value), {
+        message: t("numberIsNotAllowed"),
+      })
+      .refine((val) => !val.includes("@"), {
+        message: t("cannotContainEmail"),
+      })
+      .regex(/^[a-zA-Zآ-ی]+(\s[a-zA-Zآ-ی]+)*$/, {
+        message: t("invalidNameCharacters"),
+      }),
+
+    lastName: z
+      .string()
+      .min(1, { message: t("lastNamerequired") })
+      .refine((value) => !/^\d+$/.test(value), {
+        message: t("numberIsNotAllowed"),
+      })
+      .refine((val) => !val.includes("@"), {
+        message: t("cannotContainEmail"),
+      })
+      .regex(/^[a-zA-Zآ-ی]+(\s[a-zA-Zآ-ی]+)*$/, {
+        message: t("invalidLastNameCharacters"),
+      }),
+
+    jobTitle: z
+      .string()
+      .min(1, { message: t("jobTitlerequired") })
+      .refine((value) => !/^\d+$/.test(value), {
+        message: t("numberIsNotAllowed"),
+      })
+      .refine((val) => !val.includes("@"), {
+        message: t("cannotContainEmail"),
+      })
+      .regex(/^[a-zA-Zآ-ی]+([-\s_][a-zA-Zآ-ی]+)*$/, {
+        message: t("invalidJobTitleCharacters"),
+      }),
     sex: z
       .string()
       .refine(
@@ -36,7 +71,14 @@ export const createBasicInfoSchema = (t: TFunction<string, undefined>) => {
           message: t("militaryServiceStatusMessage"),
         },
       ),
-    email: z.email({ message: t("invalidEmail") }),
+    email: z
+      .email({ message: t("invalidEmail") })
+      .min(5, { message: t("invalidEmail") })
+      .max(124, { message: t("emailTooLong") })
+      .refine((val) => !/\s/.test(val), { message: t("emailNoSpacesAllowed") })
+      .refine((val) => /^[^\u0600-\u06FF]+$/.test(val), {
+        message: t("emailNoPersianCharacters"),
+      }),
 
     birthday: z.object({
       day: z.string().min(1, t("birthdayDayRequired")),
@@ -44,24 +86,88 @@ export const createBasicInfoSchema = (t: TFunction<string, undefined>) => {
       year: z.string().min(1, t("birthdayYearRequired")),
     }),
 
-    mobileNumber: z.string().regex(/^(09\d{9}|\+989\d{9}|0989\d{9})$/, {
-      message: t("invalidMobile"),
-    }),
+    mobileNumber: z
+      .string()
+      .min(11, { message: t("min11charMobile") })
+      .refine((val) => !val.includes("@"), {
+        message: t("cannotContainEmail"),
+      })
+      .regex(/^(09\d{9}|\+989\d{9}|0989\d{9})$/, {
+        message: t("invalidMobile"),
+      })
+      .refine((val) => !/[\u0600-\u06FF]/.test(val), {
+        message: t("noPersianCharacters"),
+      }),
 
-    phone: z.string().min(10, { message: t("invalidMobile") }),
+    phone: z
+      .string()
+      .min(8, { message: t("invalidPhone") })
+      .refine((val) => !val.includes("@"), {
+        message: t("cannotContainEmail"),
+      })
+      .refine((val) => !/[\u0600-\u06FF]/.test(val), {
+        message: t("noPersianCharacters"),
+      }),
 
-    country: z.string().min(1, { message: t("countryRequired") }),
-    province: z.string().min(1, { message: t("provinceRequired") }),
-    city: z.string().min(1, { message: t("cityRequired") }),
+    country: z
+      .string()
+      .min(1, { message: t("countryRequired") })
+      .refine((val) => !val.includes("@"), {
+        message: t("cannotContainEmail"),
+      }),
+    province: z
+      .string()
+      .min(1, { message: t("provinceRequired") })
+      .refine((val) => !val.includes("@"), {
+        message: t("cannotContainEmail"),
+      }),
+    city: z
+      .string()
+      .min(1, { message: t("cityRequired") })
+      .refine((val) => !val.includes("@"), {
+        message: t("cannotContainEmail"),
+      }),
     address: z
       .string()
       .min(5, { message: t("addressFiveCharacters") })
-      .max(100, { message: "addressHundredCharacters" }),
+      .max(100, { message: "addressHundredCharacters" })
+      .refine((value) => /[a-zA-Zآ-ی]/.test(value), {
+        message: t("addressMustContainLetters"),
+      })
+      .refine((val) => !val.includes("@"), {
+        message: t("cannotContainEmail"),
+      }),
+
     webSite: z
       .string()
-      .url({ message: t("invalidUrl") })
-      .or(z.literal(""))
-      .optional(),
+      .trim()
+      .optional()
+      .transform((val) => {
+        if (!val) return "";
+
+        if (!val.startsWith("http://") && !val.startsWith("https://")) {
+          return `https://${val}`;
+        }
+
+        return val;
+      })
+      // ❌ no Persian
+      .refine((val) => !/[\u0600-\u06FF]/.test(val), {
+        message: t("urlNoPersianCharacters"),
+      })
+      .refine(
+        (val) => {
+          if (!val) return true;
+
+          const domainRegex = /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/;
+
+          return domainRegex.test(val);
+        },
+        {
+          message: t("invalidUrl"),
+        },
+      ),
+
     summary: z.string().optional(),
   });
 };
