39a3471ed2416f8aaff439bf472d0bea7e633709
AMIRALI <alimoradi7718@gmail.com>
2026-06-10 10:58:42 +0330

    add related-property for projects-fileds

--- src/features/resume/components/steps/projectsStep/index.tsx
+++ src/features/resume/components/steps/projectsStep/index.tsx
@@ -1,5 +1,42 @@
+import { useTranslation } from "@/lib/i18n/client";
+import { useLang } from "@/provider/lngProvider";
+import { useFieldArray } from "react-hook-form";
+import { ProjectItem } from "./components/ProjectItem";
+import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
+import { CustomButton } from "@/components/ui/CustomButton";
+import { ResumeFormValues } from "@/features/resume/schemas/resume.schema";
+
 function ProjectsStep() {
-  return <div>projectsStep</div>;
+  const { lng } = useLang();
+  const { t } = useTranslation(lng, "form");
+
+  const { fields, append, remove } = useFieldArray<
+    ResumeFormValues,
+    "projects"
+  >({
+    name: "projects",
+  });
+
+  const onRowAdd = () => {
+    append({
+      clientName: "",
+      description: "",
+      projectMonth: "",
+      projectTitle: "",
+      projectUrl: "",
+      projectYear: "",
+    });
+  };
+  return (
+    <div>
+      {fields.map((field, index) => (
+        <CustomResumeCardComponents calssName="mt-2" key={field.id}>
+          <ProjectItem lng={lng} t={t} index={index} onDelete={remove} />
+        </CustomResumeCardComponents>
+      ))}
+      <CustomButton onClick={onRowAdd}>ADD</CustomButton>
+    </div>
+  );
 }
 
 export { ProjectsStep };
