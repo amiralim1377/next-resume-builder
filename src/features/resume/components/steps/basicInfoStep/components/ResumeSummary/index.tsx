6408f138a0ac07759e1118cfbbda24490377ad67
AMIRALI <alimoradi7718@gmail.com>
2026-06-15 19:29:28 +0330

    add CustomControlledResumeSummary to ResumeSummary

--- src/features/resume/components/steps/basicInfoStep/components/ResumeSummary/index.tsx
+++ src/features/resume/components/steps/basicInfoStep/components/ResumeSummary/index.tsx
@@ -1,9 +1,22 @@
+import { CustomControlledResumeSummary } from "@/components/ui/CustomControlledResumeSummary";
 import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
+import { Language } from "@/lib/i18n/settings";
+import { TFunction } from "i18next";
 
-// type ResumeSummaryProps = {};
+type ResumeSummaryProps = {
+  t: TFunction<string, undefined>;
+  lng: Language;
+};
 
-const ResumeSummary = () => {
-  return <CustomResumeCardComponents>ResumeSummary</CustomResumeCardComponents>;
+const ResumeSummary = ({}: ResumeSummaryProps) => {
+  return (
+    <CustomResumeCardComponents>
+      <CustomControlledResumeSummary
+        label="summary"
+        name={`basicInfo.summary`}
+      />
+    </CustomResumeCardComponents>
+  );
 };
 
 export { ResumeSummary };
