00cd7b7f32a118111669e8ce6be8c0e0f267f01d
AMIRALI <alimoradi7718@gmail.com>
2026-06-17 12:49:40 +0330

    add UserProfileSection instead of ProfilePhotoUploader

--- src/features/resume/components/steps/basicInfoStep/index.tsx
+++ src/features/resume/components/steps/basicInfoStep/index.tsx
@@ -4,14 +4,14 @@ import { ContactInformation } from "./components/ContactInformationSection";
 import { ResumeSummary } from "./components/ResumeSummary";
 import { useLang } from "@/provider/lngProvider";
 import { useTranslation } from "@/lib/i18n/client";
-import { ProfilePhotoUploader } from "./components/ProfilePhotoUploader";
+import { UserProfileSection } from "./components/UserProfileSection";
 
 function BasicInfoStep() {
   const { lng } = useLang();
   const { t } = useTranslation(lng, "form");
   return (
     <div className="flex w-full flex-col space-y-8">
-      <ProfilePhotoUploader t={t} />
+      <UserProfileSection t={t} />
       <BasicInfoSection lng={lng} t={t} />
       <ContactInformation t={t} />
       <ResumeSummary lng={lng} t={t} />
