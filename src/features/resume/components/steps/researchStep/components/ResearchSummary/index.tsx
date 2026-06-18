3419609e89fffee98acb098a2fadb57e6113234a
AMIRALI <alimoradi7718@gmail.com>
2026-06-18 15:54:07 +0330

    add ResearchSummary

---
+++ src/features/resume/components/steps/researchStep/components/ResearchSummary/index.tsx
@@ -0,0 +1,20 @@
+import { CustomControlledResumeSummary } from "@/components/ui/CustomControlledResumeSummary";
+import { TFunction } from "i18next";
+
+type ResearchSummaryProps = {
+  t: TFunction<string, undefined>;
+  index: number;
+};
+
+const ResearchSummary = ({ index, t }: ResearchSummaryProps) => {
+  return (
+    <CustomControlledResumeSummary
+      label="summary"
+      name={`research.${index}.summary`}
+      description={t("researchSummaryDescription")}
+      descriptionClassName="text-text-secondary my-2 text-xs"
+    />
+  );
+};
+
+export { ResearchSummary };
