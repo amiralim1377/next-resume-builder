9813fa636b4462af763c450230e70055513eede8
AMIRALI <alimoradi7718@gmail.com>
2026-06-20 21:16:00 +0330

    add label property to CustomResumeCardComponents

--- src/components/ui/CustomResumeCardComponents/index.tsx
+++ src/components/ui/CustomResumeCardComponents/index.tsx
@@ -1,28 +1,48 @@
 import { cn } from "@/utils/cn";
 import { CSSProperties, ReactNode } from "react";
 
+type CardClassName = {
+  labelClassName: string;
+  cardClassName: string;
+};
+
 type CustomResumeCardComponentsProps = {
   children: ReactNode;
   className?: string;
+  classNames?: CardClassName;
+
   style?: CSSProperties;
+  label?: ReactNode;
+  labelClassName?: string;
 };
 
 const CustomResumeCardComponents = ({
   children,
   className,
+  classNames,
   style,
+  label,
   ...props
 }: CustomResumeCardComponentsProps) => {
   return (
     <div
       className={cn(
-        "border-ui-border w-full rounded-lg border p-5 shadow-lg",
+        "flex flex-col items-start justify-between gap-3",
         className,
       )}
-      style={style}
-      {...props}
     >
-      {children}
+      <div className={cn(classNames?.labelClassName)}>{label}</div>
+
+      <div
+        className={cn(
+          "border-ui-border w-full rounded-lg border p-5 shadow-lg",
+          classNames?.cardClassName,
+        )}
+        style={style}
+        {...props}
+      >
+        {children}
+      </div>
     </div>
   );
 };
--- src/features/resume/components/steps/basicInfoStep/components/BasicInfoSection/index.tsx
+++ src/features/resume/components/steps/basicInfoStep/components/BasicInfoSection/index.tsx
@@ -4,6 +4,9 @@ import { BasicInformation } from "../BasicInformation";
 import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
 import { TFunction } from "i18next";
 import { Language } from "@/lib/i18n/settings";
+import { CustomLabel } from "@/components/ui/CustomLabel";
+import { FileUser } from "lucide-react";
+import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
 
 type BasicInfoSectionProps = {
   t: TFunction<string, undefined>;
@@ -11,9 +14,18 @@ type BasicInfoSectionProps = {
 };
 
 function BasicInfoSection({ t, lng }: BasicInfoSectionProps) {
+  const { colors } = useThemeColors();
   return (
     <CustomResumeCardComponents
-      className={"flex flex-col items-start justify-between"}
+      label={
+        <CustomLabel
+          size="xl"
+          variant="bold"
+          icon={<FileUser color={colors.brand?.brandPrimary} />}
+        >
+          {t("personalInfo")}
+        </CustomLabel>
+      }
     >
       <BasicInformation lng={lng} t={t} />
     </CustomResumeCardComponents>
