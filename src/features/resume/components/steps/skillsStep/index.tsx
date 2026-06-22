fad8accb014f0cd63c26acaa821ef6af70e6f6ca
AMIRALI <alimoradi7718@gmail.com>
2026-06-22 20:45:19 +0330

    add div with related style-className

--- src/features/resume/components/steps/skillsStep/index.tsx
+++ src/features/resume/components/steps/skillsStep/index.tsx
@@ -8,11 +8,11 @@ function SkillsStep() {
   const { lng } = useLang();
   const { t } = useTranslation(lng, "form");
   return (
-    <>
+    <div className="flex w-full flex-col space-y-2.5">
       <LanguageSection t={t} lng={lng} />
       <CoreSkillsSection t={t} lng={lng} />
       <CoursesCertificationsSection t={t} lng={lng} />
-    </>
+    </div>
   );
 }
 
