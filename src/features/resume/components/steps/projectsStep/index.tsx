27f9f6fc736bb626ce875a48881b40cead551685
AMIRALI <alimoradi7718@gmail.com>
2026-06-22 20:44:44 +0330

    feat: add relevant summary properties

--- src/features/resume/components/steps/projectsStep/index.tsx
+++ src/features/resume/components/steps/projectsStep/index.tsx
@@ -2,13 +2,25 @@ import { useTranslation } from "@/lib/i18n/client";
 import { useLang } from "@/provider/lngProvider";
 import { useFieldArray } from "react-hook-form";
 import { ProjectItem } from "./components/ProjectItem";
-import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
 import { CustomButton } from "@/components/ui/CustomButton";
 import { ResumeFormValues } from "@/features/resume/schemas/resume.schema";
+import { CustomLabel } from "@/components/ui/CustomLabel";
+import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
+import { FolderKanban } from "lucide-react";
+import { useEffect, useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/NewCustomAccordion";
+import { ProjectAccordionHeader } from "./components/ProjectAccordionHeader";
 
 function ProjectsStep() {
+  const [activeAccordionId, setActiveAccordionId] = useState<string>("");
   const { lng } = useLang();
   const { t } = useTranslation(lng, "form");
+  const { colors } = useThemeColors();
 
   const { fields, append, remove } = useFieldArray<
     ResumeFormValues,
@@ -20,21 +32,69 @@ function ProjectsStep() {
   const onRowAdd = () => {
     append({
       clientName: "",
-      summary: "",
+      summary: {
+        type: "",
+        content: [],
+      },
       projectMonth: "",
       projectTitle: "",
       projectUrl: "",
       projectYear: "",
     });
   };
+
+  useEffect(() => {
+    if (fields.length > 0) {
+      const lastFieldId = fields[fields.length - 1].id;
+      // eslint-disable-next-line
+      setActiveAccordionId(lastFieldId);
+    }
+  }, [fields.length]);
+
   return (
-    <div>
-      {fields.map((field, index) => (
-        <CustomResumeCardComponents calssName="mt-2" key={field.id}>
-          <ProjectItem lng={lng} t={t} index={index} onDelete={remove} />
-        </CustomResumeCardComponents>
-      ))}
-      <CustomButton onClick={onRowAdd}>ADD</CustomButton>
+    <div className="flex w-full flex-col space-y-2.5">
+      <CustomLabel
+        size="lg"
+        variant="bold"
+        icon={<FolderKanban color={colors.brand?.brandPrimary} />}
+      >
+        {t("projects")}
+      </CustomLabel>
+
+      {fields.length === 0 ? (
+        <div className="py-4 text-center text-sm opacity-60">
+          No lng added yet
+        </div>
+      ) : (
+        <Accordion
+          type="single"
+          collapsible
+          className="w-full space-y-2 rounded-lg p-5 shadow-lg"
+          value={activeAccordionId}
+          onValueChange={setActiveAccordionId}
+        >
+          {fields.map((field, index) => {
+            return (
+              <AccordionItem key={field.id} value={field.id}>
+                <AccordionTrigger>
+                  <ProjectAccordionHeader index={index} />
+                </AccordionTrigger>
+                <AccordionContent className="p-4">
+                  <ProjectItem
+                    lng={lng}
+                    t={t}
+                    index={index}
+                    onDelete={remove}
+                  />
+                </AccordionContent>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      )}
+      <CustomButton className="w-full" onClick={onRowAdd}>
+        ADD
+      </CustomButton>
     </div>
   );
 }
--- src/features/resume/components/steps/researchStep/index.tsx
+++ src/features/resume/components/steps/researchStep/index.tsx
@@ -4,11 +4,23 @@ import { useTranslation } from "@/lib/i18n/client";
 import { useLang } from "@/provider/lngProvider";
 import { useFieldArray } from "react-hook-form";
 import { ResearchItem } from "./components/ResearchItem";
-import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
+import { CustomLabel } from "@/components/ui/CustomLabel";
+import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
+import { PenLine } from "lucide-react";
+import { useEffect, useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/NewCustomAccordion";
+import { ResearchAccordionHeader } from "./components/ResearchAccordionHeader";
 
 function ResearchStep() {
+  const [activeAccordionId, setActiveAccordionId] = useState<string>("");
   const { lng } = useLang();
   const { t } = useTranslation(lng, "form");
+  const { colors } = useThemeColors();
   const { fields, append, remove } = useFieldArray<
     ResumeFormValues,
     "research"
@@ -23,6 +35,10 @@ function ResearchStep() {
       publisher: "",
       researchTitle: "",
       researchUrl: "",
+      summary: {
+        type: "",
+        content: [],
+      },
     });
   };
 
@@ -30,23 +46,57 @@ function ResearchStep() {
     remove(index);
   };
 
+  useEffect(() => {
+    if (fields.length > 0) {
+      const lastFieldId = fields[fields.length - 1].id;
+      // eslint-disable-next-line
+      setActiveAccordionId(lastFieldId);
+    }
+  }, [fields.length]);
+
   return (
-    <>
-      <div className="flex w-full flex-col space-y-8">
-        {fields.map((field, index) => (
-          <CustomResumeCardComponents calssName="mt-2" key={field.id}>
-            <ResearchItem
-              t={t}
-              lng={lng}
-              onDelete={onDelete}
-              key={field.id}
-              index={index}
-            />
-          </CustomResumeCardComponents>
-        ))}
-      </div>
+    <div className="flex w-full flex-col space-y-2.5">
+      <CustomLabel
+        size="lg"
+        variant="bold"
+        icon={<PenLine color={colors.brand?.brandPrimary} />}
+      >
+        {t("researchAndPublications")}
+      </CustomLabel>
+      {fields.length === 0 ? (
+        <div className="py-4 text-center text-sm opacity-60">
+          No lng added yet
+        </div>
+      ) : (
+        <Accordion
+          type="single"
+          collapsible
+          className="w-full space-y-2 rounded-lg p-5 shadow-lg"
+          value={activeAccordionId}
+          onValueChange={setActiveAccordionId}
+        >
+          {fields.map((field, index) => {
+            return (
+              <AccordionItem key={field.id} value={field.id}>
+                <AccordionTrigger>
+                  <ResearchAccordionHeader index={index} />
+                </AccordionTrigger>
+                <AccordionContent className="p-4">
+                  <ResearchItem
+                    t={t}
+                    lng={lng}
+                    onDelete={onDelete}
+                    key={field.id}
+                    index={index}
+                  />
+                </AccordionContent>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      )}
       <CustomButton onClick={onRowAdd}>ADD</CustomButton>
-    </>
+    </div>
   );
 }
 
