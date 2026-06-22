c8cafb87af49fcef79f2fab99f19238537d92fbb
AMIRALI <alimoradi7718@gmail.com>
2026-06-22 20:40:54 +0330

    add proper and relevant properties for summary

--- src/features/resume/components/steps/educationStep/index.tsx
+++ src/features/resume/components/steps/educationStep/index.tsx
@@ -1,13 +1,26 @@
 import { useLang } from "@/provider/lngProvider";
-import { EducationSection } from "./components/EducationSection";
 import { useTranslation } from "@/lib/i18n/client";
 import { CustomButton } from "@/components/ui/CustomButton";
-import { useFieldArray } from "react-hook-form";
+import { useFieldArray, useWatch } from "react-hook-form";
 import { ResumeFormValues } from "@/features/resume/schemas/resume.schema";
+import { CustomLabel } from "@/components/ui/CustomLabel";
+import { GraduationCap } from "lucide-react";
+import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/NewCustomAccordion";
+import { useEffect, useState } from "react";
+import { EducationItem } from "./components/EducationItem";
+import { EducationAccordionHeader } from "./components/EducationAccordionHeader";
 
 function EducationStep() {
+  const [activeAccordionId, setActiveAccordionId] = useState<string>("");
   const { lng } = useLang();
   const { t } = useTranslation(lng, "form");
+  const { colors } = useThemeColors();
   const { fields, append, remove } = useFieldArray<
     ResumeFormValues,
     "education"
@@ -30,25 +43,69 @@ function EducationStep() {
       graduationMonth: "",
       graduationYear: "",
       isStudyingNow: false,
-      summary: "",
+      summary: {
+        type: "",
+        content: [],
+      },
     });
   };
+
+  useEffect(() => {
+    if (fields.length > 0) {
+      const lastFieldId = fields[fields.length - 1].id;
+      // eslint-disable-next-line
+      setActiveAccordionId(lastFieldId);
+    }
+  }, [fields.length]);
 
   const onDelete = (index: number) => {
     remove(index);
   };
   return (
-    <div className="flex w-full flex-col space-y-8">
-      {fields.map((field, index) => (
-        <EducationSection
-          onDelete={onDelete}
-          key={field.id}
-          index={index}
-          t={t}
-          lng={lng}
-        />
-      ))}
-      <CustomButton onClick={onRowAdd}>ADD</CustomButton>
+    <div className="flex w-full flex-col space-y-2.5">
+      <CustomLabel
+        size="lg"
+        variant="bold"
+        icon={<GraduationCap color={colors.brand?.brandPrimary} />}
+      >
+        {t("academicHistory")}
+      </CustomLabel>
+
+      {fields.length === 0 ? (
+        <div className="py-4 text-center text-sm opacity-60">
+          No lng added yet
+        </div>
+      ) : (
+        <Accordion
+          type="single"
+          collapsible
+          className="w-full space-y-2 rounded-lg p-5 shadow-lg"
+          value={activeAccordionId}
+          onValueChange={setActiveAccordionId}
+        >
+          {fields.map((field, index) => {
+            return (
+              <AccordionItem key={field.id} value={field.id}>
+                <AccordionTrigger>
+                  <EducationAccordionHeader index={index} t={t} />
+                </AccordionTrigger>
+                <AccordionContent className="p-4">
+                  <EducationItem
+                    index={index}
+                    onDelete={onDelete}
+                    t={t}
+                    lng={lng}
+                  />
+                </AccordionContent>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      )}
+
+      <CustomButton className="" onClick={onRowAdd}>
+        ADD
+      </CustomButton>
     </div>
   );
 }
