77b57e7433caa32702d817f4c7d231493312261c
AMIRALI <alimoradi7718@gmail.com>
2026-06-22 20:46:52 +0330

    add new Accordion and remove old-rendering-way of CoursesCertificationsItem and add useEffect to mange open item when a new row were added

--- src/features/resume/components/steps/skillsStep/components/CoursesCertificationsSection/index.tsx
+++ src/features/resume/components/steps/skillsStep/components/CoursesCertificationsSection/index.tsx
@@ -1,20 +1,41 @@
-import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
 import { Language } from "@/lib/i18n/settings";
 import { TFunction } from "i18next";
 import { useFieldArray } from "react-hook-form";
 import { CoursesCertificationsItem } from "./components/CoursesCertificationsItem";
 import { CustomButton } from "@/components/ui/CustomButton";
 import { ResumeFormValues } from "@/features/resume/schemas/resume.schema";
+import { CustomLabel } from "@/components/ui/CustomLabel";
+import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
+import { BadgeCheck } from "lucide-react";
+import { useEffect, useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/NewCustomAccordion";
+import { CoursesCertificationsAccordionHeader } from "./components/CoursesCertificationsAccordionHeader";
 
 type CoursesCertificationsSectionProps = {
   t: TFunction<string, undefined>;
   lng: Language;
 };
 
+const DEFAULT_COURSE_CERTIFICATION: ResumeFormValues["coursesAndCertifications"][number] =
+  {
+    coursesAndCertificationsName: "",
+    instituteName: "",
+    certificateIssueMonth: "",
+    certificateIssueYear: "",
+    certificateUrl: "",
+  };
+
 const CoursesCertificationsSection = ({
   lng,
   t,
 }: CoursesCertificationsSectionProps) => {
+  const [activeAccordionId, setActiveAccordionId] = useState<string>("");
+  const { colors } = useThemeColors();
   const { fields, append, remove } = useFieldArray<
     ResumeFormValues,
     "coursesAndCertifications"
@@ -23,38 +44,67 @@ const CoursesCertificationsSection = ({
   });
 
   const addCoursesCertifications = () => {
-    append({
-      coursesAndCertificationsName: "",
-      instituteName: "",
-      certificateIssueMonth: "",
-      certificateIssueYear: "",
-      certificateUrl: "",
-    });
+    append(DEFAULT_COURSE_CERTIFICATION);
   };
 
+  useEffect(() => {
+    if (fields.length > 0) {
+      const lastFieldId = fields[fields.length - 1].id;
+      // eslint-disable-next-line
+      setActiveAccordionId(lastFieldId);
+    }
+  }, [fields.length]);
+
   return (
-    <>
-      {fields.map((field, index) => {
-        return (
-          <CustomResumeCardComponents key={field.id}>
-            <CoursesCertificationsItem
-              lng={lng}
-              t={t}
-              index={index}
-              onDelete={remove}
-            />
-          </CustomResumeCardComponents>
-        );
-      })}
+    <div className="flex w-full flex-col space-y-2.5">
+      <CustomLabel
+        size="lg"
+        variant="bold"
+        icon={<BadgeCheck color={colors.brand?.brandPrimary} />}
+      >
+        {t("trainingAndCertificates")}
+      </CustomLabel>
+
+      {fields.length === 0 ? (
+        <div className="py-4 text-center text-sm opacity-60">
+          No lng added yet
+        </div>
+      ) : (
+        <Accordion
+          type="single"
+          collapsible
+          className="w-full space-y-2 rounded-lg p-5 shadow-lg"
+          value={activeAccordionId}
+          onValueChange={setActiveAccordionId}
+        >
+          {fields.map((field, index) => {
+            return (
+              <AccordionItem key={field.id} value={field.id}>
+                <AccordionTrigger>
+                  <CoursesCertificationsAccordionHeader index={index} />
+                </AccordionTrigger>
+                <AccordionContent className="p-4">
+                  <CoursesCertificationsItem
+                    lng={lng}
+                    t={t}
+                    index={index}
+                    onDelete={remove}
+                  />
+                </AccordionContent>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      )}
 
       <CustomButton
-        className="my-4 w-full"
+        className="w-full"
         type="button"
         onClick={addCoursesCertifications}
       >
         add
       </CustomButton>
-    </>
+    </div>
   );
 };
 
