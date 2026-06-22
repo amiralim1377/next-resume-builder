d0e0c084c4cdbdbb3749cdc35d180bf58b6096d3
AMIRALI <alimoradi7718@gmail.com>
2026-06-22 20:39:49 +0330

    pass label props to CustomResumeCardComponents and remove extra image-watch

--- src/features/resume/components/steps/basicInfoStep/components/UserProfileSection/index.tsx
+++ src/features/resume/components/steps/basicInfoStep/components/UserProfileSection/index.tsx
@@ -1,19 +1,30 @@
 import { TFunction } from "i18next";
 import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
-import { useWatch } from "react-hook-form";
 import { CustomControlledPhotoUploader } from "@/components/ui/CustomControlledPhotoUploader";
+import { CustomLabel } from "@/components/ui/CustomLabel";
+import { ImageUp } from "lucide-react";
+import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
 
 type UserProfileSectionProps = {
   t: TFunction<string, undefined>;
 };
 
 const UserProfileSection = ({ t }: UserProfileSectionProps) => {
-  const image = useWatch({
-    name: "profileImage",
-  });
+  const { colors } = useThemeColors();
 
   return (
-    <CustomResumeCardComponents calssName="w-full">
+    <CustomResumeCardComponents
+      className="w-full"
+      label={
+        <CustomLabel
+          size="lg"
+          variant="bold"
+          icon={<ImageUp color={colors.brand?.brandPrimary} />}
+        >
+          {t("uploadResumePhoto")}
+        </CustomLabel>
+      }
+    >
       <CustomControlledPhotoUploader t={t} label="hi" name="profileImage" />
     </CustomResumeCardComponents>
   );
