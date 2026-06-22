dfade09a7e73cb0db965c135584d68554174f9bd
AMIRALI <alimoradi7718@gmail.com>
2026-06-22 20:38:50 +0330

    pass label props to CustomResumeCardComponents

--- src/features/resume/components/steps/basicInfoStep/components/ContactInformationSection/index.tsx
+++ src/features/resume/components/steps/basicInfoStep/components/ContactInformationSection/index.tsx
@@ -2,14 +2,29 @@ import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComp
 import { ContactDetails } from "../ContactDetails";
 import { LocationDetails } from "../LocationDetails";
 import { TFunction } from "i18next";
+import { CustomLabel } from "@/components/ui/CustomLabel";
+import { Contact } from "lucide-react";
+import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
 
 type ContactInformationProps = {
   t: TFunction<string, undefined>;
 };
 
 const ContactInformation = ({ t }: ContactInformationProps) => {
+  const { colors } = useThemeColors();
+
   return (
-    <CustomResumeCardComponents>
+    <CustomResumeCardComponents
+      label={
+        <CustomLabel
+          size="lg"
+          variant="bold"
+          icon={<Contact color={colors.brand?.brandPrimary} />}
+        >
+          {t("contactInformation")}
+        </CustomLabel>
+      }
+    >
       <ContactDetails t={t} />
       <LocationDetails t={t} />
     </CustomResumeCardComponents>
