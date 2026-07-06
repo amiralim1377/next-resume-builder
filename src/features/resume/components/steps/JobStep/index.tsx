28a39ed0c9ea6bf015586f9e46cb22f2cff7f7b9
AMIRALI <alimoradi7718@gmail.com>
2026-07-06 13:06:49 +0330

    feat: Refactor steps to use ArrayFieldStep for dynamic form handling

--- src/features/resume/components/steps/JobStep/index.tsx
+++ src/features/resume/components/steps/JobStep/index.tsx
@@ -1,111 +1,78 @@
 import { useLang } from "@/provider/lngProvider";
 import { useTranslation } from "@/lib/i18n/client";
-import { JobItem } from "./components/JobItem";
-import { useFieldArray } from "react-hook-form";
 import { ResumeFormValues } from "@/features/resume/schemas/resume.schema";
-import { CustomButton } from "@/components/ui/CustomButton";
 import { CustomLabel } from "@/components/ui/CustomLabel";
-import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
 import { Briefcase } from "lucide-react";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/NewCustomAccordion";
-import { useEffect, useRef, useState } from "react";
-import { JobAccordionHeader } from "./components/JobAccordionHeader";
+import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
 import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
+import { JobAccordionHeader } from "./components/JobAccordionHeader";
+import { JobItem } from "./components/JobItem";
+import { EmptyStep } from "../EmptyStep";
+import { ArrayFieldStep } from "../../ArrayFieldStep";
 
 function JobStep() {
-  const [activeAccordionId, setActiveAccordionId] = useState<string>("");
   const { lng } = useLang();
   const { t } = useTranslation(lng, "form");
   const { colors } = useThemeColors();
-  const { fields, append, remove } = useFieldArray<ResumeFormValues, "job">({
-    name: "job",
-  });
-  const prevLengthRef = useRef(fields.length);
 
-  const onRowAdd = () => {
-    append({
-      status: "empty",
-      jobTitle: "",
-      companyName: "",
-      country: "",
-      province: "",
-      city: "",
-      entryDate: "",
-      employmentEndYearDate: "",
-      isCurrentlyWorkingHere: false,
-      summary: {
-        type: "",
-        content: [],
-      },
-    });
-  };
-
-  useEffect(() => {
-    const isRowAdded = fields.length > prevLengthRef.current;
-    if (isRowAdded && fields.length > 0) {
-      const lastFieldId = fields[fields.length - 1].id;
-      setActiveAccordionId(lastFieldId);
-    }
-    prevLengthRef.current = fields.length;
-  }, [fields]);
-
-  const onDelete = (index: number) => {
-    remove(index);
+  // Bulletproof typing to prevent status string widening
+  const defaultObj: ResumeFormValues["job"][number] = {
+    status: "empty",
+    jobTitle: "",
+    companyName: "",
+    country: "",
+    province: "",
+    city: "",
+    entryDate: "",
+    employmentEndYearDate: "",
+    isCurrentlyWorkingHere: false,
+    summary: {
+      type: "",
+      content: [],
+    },
   };
 
   return (
     <div className="flex w-full flex-col space-y-2.5">
-      <CustomResumeCardComponents>
+      <CustomResumeCardComponents className="flex w-full flex-col space-y-2.5">
         <CustomLabel
           size="lg"
           variant="bold"
+          description={t("addJobDescription")}
+          descriptionSize="md"
           icon={<Briefcase color={colors.brand?.brandPrimary} />}
+          divider
+          dividerClassName={"pb-3"}
         >
           {t("careerHistory")}
         </CustomLabel>
 
-        {fields.length === 0 ? (
-          <div className="py-4 text-center text-sm opacity-60">
-            No lng added yet
-          </div>
-        ) : (
-          <Accordion
-            type="single"
-            collapsible
-            className="w-full space-y-2 rounded-lg p-5 shadow-lg"
-            value={activeAccordionId}
-            onValueChange={setActiveAccordionId}
-          >
-            {fields.map((field, index) => {
-              return (
-                <AccordionItem key={field.id} value={field.id}>
-                  <AccordionTrigger>
-                    <JobAccordionHeader index={index} t={t} />
-                  </AccordionTrigger>
-                  <AccordionContent className="p-4">
-                    <JobItem
-                      t={t}
-                      lng={lng}
-                      onDelete={onDelete}
-                      key={field.id}
-                      index={index}
-                    />
-                  </AccordionContent>
-                </AccordionItem>
-              );
-            })}
-          </Accordion>
-        )}
+        <ArrayFieldStep<ResumeFormValues>
+          fieldName="job"
+          addButtonLabel={t("add")}
+          emptyRowValues={defaultObj}
+          renderEmptyState={(append) => (
+            <EmptyStep
+              iconSize={32}
+              iconColor="text-brandLight"
+              icon={Briefcase}
+              title={t("noJobAddedYet")}
+              description={t("emptyStepJobDescription")}
+              buttonLabel={t("addJob")}
+              onClick={append}
+            />
+          )}
+          renderHeader={(index) => <JobAccordionHeader index={index} t={t} />}
+          renderItem={(index, remove) => (
+            <JobItem
+              index={index}
+              onDelete={() => remove(index)}
+              t={t}
+              lng={lng}
+            />
+          )}
+        />
       </CustomResumeCardComponents>
-
-      <CustomButton type="button" onClick={onRowAdd}>
-        ADD
-      </CustomButton>
     </div>
   );
 }
--- src/features/resume/components/steps/educationStep/index.tsx
+++ src/features/resume/components/steps/educationStep/index.tsx
@@ -1,71 +1,39 @@
 import { useLang } from "@/provider/lngProvider";
 import { useTranslation } from "@/lib/i18n/client";
-import { CustomButton } from "@/components/ui/CustomButton";
-import { useFieldArray } from "react-hook-form";
 import { ResumeFormValues } from "@/features/resume/schemas/resume.schema";
 import { CustomLabel } from "@/components/ui/CustomLabel";
 import { GraduationCap } from "lucide-react";
 import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/NewCustomAccordion";
-import { useEffect, useRef, useState } from "react";
-
 import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
 import { EducationAccordionHeader } from "./components/EducationAccordionHeader";
 import { EducationItem } from "./components/EducationItem";
 import { EmptyStep } from "../EmptyStep";
+import { ArrayFieldStep } from "../../ArrayFieldStep";
 
 function EducationStep() {
-  const [activeAccordionId, setActiveAccordionId] = useState<string>("");
   const { lng } = useLang();
   const { t } = useTranslation(lng, "form");
   const { colors } = useThemeColors();
-  const { fields, append, remove } = useFieldArray<
-    ResumeFormValues,
-    "education"
-  >({
-    name: "education",
-  });
 
-  const prevLengthRef = useRef(fields.length);
-
-  const onRowAdd = () => {
-    append({
-      status: "empty",
-      degreeLevel: "",
-      academicMajor: "",
-      concentration: "",
-      institutionName: "",
-      gradeAverage: "",
-      country: "",
-      province: "",
-      city: "",
-      entryDate: "",
-      graduationDate: "",
-      isStudyingNow: false,
-      summary: {
-        type: "",
-        content: [],
-      },
-    });
+  const defaultObj: ResumeFormValues["education"][number] = {
+    status: "empty",
+    degreeLevel: "",
+    academicMajor: "",
+    concentration: "",
+    institutionName: "",
+    gradeAverage: "",
+    country: "",
+    province: "",
+    city: "",
+    entryDate: "",
+    graduationDate: "",
+    isStudyingNow: false,
+    summary: {
+      type: "",
+      content: [],
+    },
   };
 
-  useEffect(() => {
-    const isRowAdded = fields.length > prevLengthRef.current;
-    if (isRowAdded && fields.length > 0) {
-      const lastFieldId = fields[fields.length - 1].id;
-      setActiveAccordionId(lastFieldId);
-    }
-    prevLengthRef.current = fields.length;
-  }, [fields]);
-
-  const onDelete = (index: number) => {
-    remove(index);
-  };
   return (
     <div className="flex w-full flex-col space-y-2.5">
       <CustomResumeCardComponents className="flex w-full flex-col space-y-2.5">
@@ -81,53 +49,34 @@ function EducationStep() {
           {t("academicHistory")}
         </CustomLabel>
 
-        {fields.length === 0 ? (
-          <EmptyStep
-            iconSize={32}
-            iconColor="text-brandLight"
-            icon={GraduationCap}
-            title={t("noEducationAddedYet")}
-            description={t("addEducationDescription")}
-            buttonLabel={t("addEducation")}
-            onClick={onRowAdd}
-          />
-        ) : (
-          <Accordion
-            type="single"
-            collapsible
-            className="w-full space-y-2 rounded-lg p-5 shadow-lg"
-            value={activeAccordionId}
-            onValueChange={setActiveAccordionId}
-          >
-            {fields.map((field, index) => {
-              return (
-                <AccordionItem
-                  className="flex w-full flex-col gap-4"
-                  key={field.id}
-                  value={field.id}
-                >
-                  <AccordionTrigger>
-                    <EducationAccordionHeader index={index} t={t} />
-                  </AccordionTrigger>
-                  <AccordionContent className="p-4">
-                    <EducationItem
-                      index={index}
-                      onDelete={onDelete}
-                      t={t}
-                      lng={lng}
-                    />
-                  </AccordionContent>
-                </AccordionItem>
-              );
-            })}
-          </Accordion>
-        )}
+        <ArrayFieldStep<ResumeFormValues>
+          fieldName="education"
+          addButtonLabel={t("add")}
+          emptyRowValues={defaultObj}
+          renderEmptyState={(append) => (
+            <EmptyStep
+              iconSize={32}
+              iconColor="text-brandLight"
+              icon={GraduationCap}
+              title={t("noEducationAddedYet")}
+              description={t("emptyStepEducationDescription")}
+              buttonLabel={t("addEducation")}
+              onClick={append}
+            />
+          )}
+          renderHeader={(index) => (
+            <EducationAccordionHeader index={index} t={t} />
+          )}
+          renderItem={(index, remove) => (
+            <EducationItem
+              index={index}
+              onDelete={() => remove(index)}
+              t={t}
+              lng={lng}
+            />
+          )}
+        />
       </CustomResumeCardComponents>
-      {fields.length !== 0 && (
-        <CustomButton type="button" className="" onClick={onRowAdd}>
-          ADD
-        </CustomButton>
-      )}
     </div>
   );
 }
