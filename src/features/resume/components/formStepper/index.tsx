12ee45aaed471ef9059ad049031d9f3d305b74ed
AMIRALI <alimoradi7718@gmail.com>
2026-07-09 11:16:08 +0330

    refactor: Simplify FormStepper by removing unused logic and replacing FormStepperItem with StepObserver

--- src/features/resume/components/formStepper/index.tsx
+++ src/features/resume/components/formStepper/index.tsx
@@ -1,61 +1,29 @@
 "use client";
-import { FormStepperItem } from "@/components/ui/CustomFormStepperItem";
-import { SectionState, StepName } from "../../types/resume.types";
-import { useFormContext, useWatch } from "react-hook-form";
-import { useMemo } from "react";
+
+import { useFormContext } from "react-hook-form";
 import { RESUME_STEPS } from "../../constants/steps";
-import { calculateStepStatus } from "../../utils/stepper.utils";
 import { useResumeFormContext } from "../ResumeFormProvider/ResumeFormContext";
+import { StepObserver } from "./StepObserver";
 
 const FormStepper = () => {
   const { handleStepClick, currentStep } = useResumeFormContext();
   const {
-    control,
     formState: { errors },
   } = useFormContext();
-  const formValues = useWatch({ control });
-
-  const stepStatuses = useMemo((): Record<StepName, SectionState> => {
-    const statuses = {} as Record<StepName, SectionState>;
-
-    RESUME_STEPS.forEach((step, index) => {
-      statuses[step.id] = calculateStepStatus({
-        step,
-        values: formValues,
-        errors,
-        index,
-      });
-    });
-
-    return statuses;
-  }, [formValues, errors]);
-
-  console.log("stepStatuses:", stepStatuses);
+  console.log("errors in formState :", errors);
 
   return (
     <div className="flex w-full justify-between">
-      {RESUME_STEPS.map((step, index) => {
-        const status = stepStatuses[step.id];
-
-        const isActive = index === currentStep;
-        const isCompleted = status === "completed";
-
-        const isPending =
-          !isActive && (status === "empty" || status === "draft");
-
-        return (
-          <FormStepperItem
-            key={step.id}
-            stepNumber={index + 1}
-            title={step.titleKey}
-            isActive={isActive}
-            isCompleted={isCompleted}
-            isPending={isPending}
-            isLast={index === RESUME_STEPS.length - 1}
-            onClick={() => handleStepClick(index)}
-          />
-        );
-      })}
+      {RESUME_STEPS.map((step, index) => (
+        <StepObserver
+          key={step.id}
+          step={step}
+          index={index}
+          isActive={index === currentStep}
+          isLast={index === RESUME_STEPS.length - 1}
+          onClick={() => handleStepClick(index)}
+        />
+      ))}
     </div>
   );
 };
