b9da274d382d2eb1257c99f786c4a36865e1d4f5
AMIRALI <alimoradi7718@gmail.com>
2026-07-12 12:37:11 +0330

    refactor: simplify BasicInfoStep and related components by removing unnecessary props

--- src/features/resume/components/Steps/BasicInfoStep/index.tsx
+++ src/features/resume/components/Steps/BasicInfoStep/index.tsx
@@ -1,21 +1,16 @@
 "use client";
-
-import { useLang } from "@/provider/lngProvider";
-import { useTranslation } from "@/lib/i18n/client";
 import { UserProfileSection } from "./components/UserProfileSection";
 import { BasicInfoSection } from "./components/BasicInfoSection";
 import { ContactInformation } from "./components/ContactInformationSection";
 import { ResumeSummary } from "./components/ResumeSummary";
 
 function BasicInfoStep() {
-  const { lng } = useLang();
-  const { t } = useTranslation(lng, "form");
   return (
     <div className="flex w-full flex-col space-y-8">
-      <UserProfileSection t={t} />
-      <BasicInfoSection lng={lng} t={t} />
-      <ContactInformation t={t} />
-      <ResumeSummary lng={lng} t={t} />
+      <UserProfileSection />
+      <BasicInfoSection />
+      <ContactInformation />
+      <ResumeSummary />
     </div>
   );
 }
