f85b3f7407f850b0c2ade2a3292672b4275ebda7
AMIRALI <alimoradi7718@gmail.com>
2026-07-13 10:29:46 +0330

    refactor: simplify calendar type handling and remove delete button from CoursesCertificationsItem

--- src/features/resume/components/Steps/SkillsStep/components/CoursesCertificationsSection/index.tsx
+++ src/features/resume/components/Steps/SkillsStep/components/CoursesCertificationsSection/index.tsx
@@ -1,108 +1,119 @@
-import { Language } from "@/lib/i18n/settings";
-import { TFunction } from "i18next";
-import { useFieldArray } from "react-hook-form";
-import { CustomButton } from "@/components/ui/CustomButton";
-import { ResumeFormValues } from "@/features/resume/schemas/resume.schema";
 import { CustomLabel } from "@/components/ui/CustomLabel";
 import { useThemeColors } from "@/provider/themeProvider/useThemeColors";
-import { BadgeCheck } from "lucide-react";
-import { useEffect, useState } from "react";
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/NewCustomAccordion";
+  ArrowDown,
+  ArrowUp,
+  BadgeCheck,
+  Briefcase,
+  CopyIcon,
+  Trash2Icon,
+} from "lucide-react";
+
+import { useLang } from "@/provider/lngProvider";
+import { useTranslation } from "@/lib/i18n/client";
+import { CoursesAndCertificationsRowValues } from "@/features/resume/schemas/CoursesAndCertificationsSchema";
+import { CustomResumeCardComponents } from "@/components/ui/CustomResumeCardComponents";
+import { cn } from "@/utils/cn";
+import { ArrayFieldStep } from "@/features/resume/components/ArrayFieldStep";
+import { ResumeFormValues } from "@/features/resume/schemas/resume.schema";
+import { EmptyStep } from "../../../EmptyStep";
+import { AccordionRowAction } from "@/features/resume/components/AccordionRowAction";
 import { CoursesCertificationsAccordionHeader } from "./components/CoursesCertificationsAccordionHeader";
 import { CoursesCertificationsItem } from "./components/CoursesCertificationsItem";
 
-type CoursesCertificationsSectionProps = {
-  t: TFunction<string, undefined>;
-  lng: Language;
-};
+const CoursesCertificationsSection = () => {
+  const { lng } = useLang();
+  const { t } = useTranslation(lng, "form");
+  const { colors } = useThemeColors();
 
-const DEFAULT_COURSE_CERTIFICATION: ResumeFormValues["coursesAndCertifications"][number] =
-  {
+  const defaultObj: CoursesAndCertificationsRowValues = {
     coursesAndCertificationsName: "",
     instituteName: "",
     certificateIssueDate: "",
     certificateUrl: "",
   };
 
-const CoursesCertificationsSection = ({
-  lng,
-  t,
-}: CoursesCertificationsSectionProps) => {
-  const [activeAccordionId, setActiveAccordionId] = useState<string>("");
-  const { colors } = useThemeColors();
-  const { fields, append, remove } = useFieldArray<
-    ResumeFormValues,
-    "coursesAndCertifications"
-  >({
-    name: "coursesAndCertifications",
-  });
-
-  const addCoursesCertifications = () => {
-    append(DEFAULT_COURSE_CERTIFICATION);
-  };
-
-  useEffect(() => {
-    if (fields.length > 0) {
-      const lastFieldId = fields[fields.length - 1].id;
-      // eslint-disable-next-line
-      setActiveAccordionId(lastFieldId);
-    }
-  }, [fields.length]);
-
   return (
     <div className="flex w-full flex-col space-y-2.5">
-      <CustomLabel
-        size="lg"
-        variant="bold"
-        icon={<BadgeCheck color={colors.brand?.brandPrimary} />}
+      <CustomResumeCardComponents
+        className={cn(
+          "flex w-full flex-col space-y-2.5",
+          "transition-colors duration-300",
+        )}
+        label={
+          <CustomLabel
+            size="lg"
+            variant="bold"
+            icon={<BadgeCheck color={colors.brand?.brandPrimary} />}
+          >
+            {t("trainingAndCertificates")}
+          </CustomLabel>
+        }
       >
-        {t("trainingAndCertificates")}
-      </CustomLabel>
-
-      {fields.length === 0 ? (
-        <div className="py-4 text-center text-sm opacity-60">
-          No lng added yet
-        </div>
-      ) : (
-        <Accordion
-          type="single"
-          collapsible
-          className="w-full space-y-2 rounded-lg p-5 shadow-lg"
-          value={activeAccordionId}
-          onValueChange={setActiveAccordionId}
-        >
-          {fields.map((field, index) => {
-            return (
-              <AccordionItem key={field.id} value={field.id}>
-                <AccordionTrigger>
-                  <CoursesCertificationsAccordionHeader index={index} />
-                </AccordionTrigger>
-                <AccordionContent className="p-4">
-                  <CoursesCertificationsItem
-                    lng={lng}
-                    t={t}
-                    index={index}
-                    onDelete={remove}
+        <ArrayFieldStep<ResumeFormValues>
+          fieldName="CoursesCertificationsSection"
+          addButtonLabel={t("add")}
+          emptyRowValues={defaultObj}
+          renderEmptyState={(append) => (
+            <EmptyStep
+              iconSize={32}
+              iconColor="text-brandLight"
+              icon={Briefcase}
+              title={t("noJobAddedYet")}
+              description={t("emptyStepJobDescription")}
+              buttonLabel={t("addJob")}
+              onClick={append}
+            />
+          )}
+          renderHeader={(index, remove, copy, move, isFirst, isLast) => (
+            <CoursesCertificationsAccordionHeader
+              index={index}
+              t={t}
+              actionsSlot={
+                <>
+                  {!isFirst && (
+                    <AccordionRowAction
+                      icon={
+                        <ArrowUp className="text-text-secondary hover:text-brandHover hover:ring-brandHover h-7 w-7 rounded-md p-1 transition-all hover:ring-2 hover:ring-offset-2" />
+                      }
+                      onClick={() => move(index, index - 1)}
+                      title={t("moveUp")}
+                    />
+                  )}
+                  {!isLast && (
+                    <AccordionRowAction
+                      icon={
+                        <ArrowDown className="text-text-secondary hover:text-brandHover hover:ring-brandHover h-7 w-7 rounded-md p-1 transition-all hover:ring-2 hover:ring-offset-2" />
+                      }
+                      onClick={() => move(index, index + 1)}
+                      title={t("moveDown")}
+                    />
+                  )}
+                  <AccordionRowAction
+                    icon={
+                      <CopyIcon className="text-text-secondary hover:text-brandHover hover:ring-brandHover h-7 w-7 rounded-md p-1 transition-all hover:ring-2 hover:ring-offset-2" />
+                    }
+                    onClick={() => copy(index)}
+                    variant="default"
+                    title={t("duplicate")}
                   />
-                </AccordionContent>
-              </AccordionItem>
-            );
-          })}
-        </Accordion>
-      )}
-
-      <CustomButton
-        className="w-full"
-        type="button"
-        onClick={addCoursesCertifications}
-      >
-        add
-      </CustomButton>
+                  <AccordionRowAction
+                    icon={
+                      <Trash2Icon className="text-text-secondary hover:text-state-error hover:ring-state-error h-7 w-7 rounded-md p-1 transition-all hover:ring-2 hover:ring-offset-2" />
+                    }
+                    onClick={() => remove(index)}
+                    variant="danger"
+                    title={t("delete")}
+                  />
+                </>
+              }
+            />
+          )}
+          renderItem={(index) => (
+            <CoursesCertificationsItem index={index} t={t} lng={lng} />
+          )}
+        />
+      </CustomResumeCardComponents>
     </div>
   );
 };
