ac1f18aca3efa41a4bdc962d71af6ed418da6142
AMIRALI <alimoradi7718@gmail.com>
2026-07-14 17:40:34 +0330

    chore: comment out skills schema and related type exports for future implementation

--- src/features/resume/schemas/resume.schema.ts
+++ src/features/resume/schemas/resume.schema.ts
@@ -4,7 +4,7 @@ import { createBasicInfoSchema } from "./BasicInfoSchema";
 import { createEducationSchema } from "./EducationSchema";
 import { createJobSchema } from "./JobSchema";
 import { createLanguageSchema } from "./LanguageSchema";
-import { createSkillsSchema } from "./SkillsSchema";
+// import { createSkillsSchema } from "./SkillsSchema";
 import { createCoursesAndCertificationsSchema } from "./CoursesAndCertificationsSchema";
 import { createResearchSchema } from "./ResearchSchema";
 import { createProfileImageSchema } from "./ProfileImageSchema";
@@ -17,7 +17,7 @@ export const createResumeSchema = (t: TFunction<string, undefined>) => {
     education: z.array(createEducationSchema(t)),
     job: z.array(createJobSchema(t)),
     languages: z.array(createLanguageSchema(t)),
-    skills: z.array(createSkillsSchema(t)),
+    // skills: z.array(createSkillsSchema(t)),
     coursesAndCertifications: z.array(createCoursesAndCertificationsSchema(t)),
 
     projects: z.array(createProjectSchema(t)),
@@ -27,11 +27,3 @@ export const createResumeSchema = (t: TFunction<string, undefined>) => {
 };
 
 export type ResumeFormValues = z.infer<ReturnType<typeof createResumeSchema>>;
-export type EducationFormValue = ResumeFormValues["education"][number];
-export type JobFormValue = ResumeFormValues["job"][number];
-export type LanguageFormValue = ResumeFormValues["languages"][number];
-export type SkillsFormValue = ResumeFormValues["skills"][number];
-export type CoursesAndCertificationsFormValue =
-  ResumeFormValues["coursesAndCertifications"][number];
-export type ProjectsFormValue = ResumeFormValues["projects"][number];
-export type ResearchFormValue = ResumeFormValues["research"][number];
