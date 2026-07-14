69fa425eed698c589a22fe4f92d1cbd7e76662ef
AMIRALI <alimoradi7718@gmail.com>
2026-07-14 17:27:39 +0330

    feat: implement query client and provider for data fetching

--- src/provider/MainProvider.tsx
+++ src/provider/MainProvider.tsx
@@ -4,6 +4,7 @@ import { LangProvider } from "./lngProvider";
 import { Language } from "@/lib/i18n/settings";
 import { PageLoadingProvider } from "./PageLoadingProvider";
 import { ThemeProvider, ThemeScheme } from "./themeProvider";
+import QueryProvider from "./queryProvider";
 
 type MainProviderProps = {
   children: ReactNode;
@@ -17,13 +18,15 @@ const MainProvider = ({
   themeFromCookie,
 }: MainProviderProps) => {
   return (
-    <LangProvider lng={lng}>
-      <PageLoadingProvider>
-        <ThemeProvider themeFromCookie={themeFromCookie}>
-          <SizeProvider>{children}</SizeProvider>
-        </ThemeProvider>
-      </PageLoadingProvider>
-    </LangProvider>
+    <QueryProvider>
+      <LangProvider lng={lng}>
+        <PageLoadingProvider>
+          <ThemeProvider themeFromCookie={themeFromCookie}>
+            <SizeProvider>{children}</SizeProvider>
+          </ThemeProvider>
+        </PageLoadingProvider>
+      </LangProvider>
+    </QueryProvider>
   );
 };
 export { MainProvider };
